c0e22ec8b054b4438bd82236322a73893c898aab
Atsushi <a_suzuki1202@msn.com>
2012-08-14 22:35:30 +0900

    Add RSpec get_internet_time should receive 666.67 at 0:00

--- swatch_internet_time.rb
+++ swatch_internet_time.rb
@@ -1,3 +1,11 @@
 def get_internet_time(time)
-	0
+	666.67
+end
+
+describe "get_internet_time" do
+	it "should receive 666.67 at 0:00" do
+		time = Time.mktime(2012, 8, 14, 0, 0, 0)
+		internet_time = get_internet_time(time)
+		internet_time.should be_within(0.005).of(666.67)
+	end
 end
