bd97d0bed8b5e22c08359ecbe1c0100a1350846d
Atsushi <a_suzuki1202@msn.com>
2012-08-14 22:51:48 +0900

    Implement calculation for internet time

--- swatch_internet_time.rb
+++ swatch_internet_time.rb
@@ -1,5 +1,11 @@
+DAY_SEC = 24 * 60 * 60
+
 def get_internet_time(time)
-	666.67
+	time_utc = time.utc
+	time_total_sec = 60 * (60 * (time_utc.hour + 1) + time_utc.min) + time_utc.sec
+	interenet_time = 1000.0 * time_total_sec / DAY_SEC
+
+	(interenet_time >= 1000) ? interenet_time - 1000 : interenet_time
 end
 
 describe "get_internet_time" do
@@ -7,5 +13,11 @@ describe "get_internet_time" do
 		time = Time.mktime(2012, 8, 14, 0, 0, 0)
 		internet_time = get_internet_time(time)
 		internet_time.should be_within(0.005).of(666.67)
+	end
+
+	it "should receive 0.0 at 8:00" do
+		time = Time.mktime(2012, 8, 14, 8, 0, 0)
+		internet_time = get_internet_time(time)
+		internet_time.should be_within(0.005).of(0.0)
 	end
 end
